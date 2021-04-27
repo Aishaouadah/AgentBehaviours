bbeafb3089e9fd5d6baff8204e23b80ef8a2c415
Aishaouadah <aicha.ouadah@etu.usthb.dz>
2021-04-27 11:34:45 +0100

    Sending a table to Agent3

--- src/Behaviour1.java
+++ src/Behaviour1.java
@@ -2,14 +2,22 @@ import jade.core.AID;
 import jade.core.behaviours.OneShotBehaviour;
 import jade.lang.acl.ACLMessage;
 
+import java.io.IOException;
+
 public class Behaviour1 extends OneShotBehaviour {
     @Override
     public void action() {
       System.out.println("this is OneShotBehaviour B1");
-      int A=10;
+      int[] A={10,4,5,7,8};
         ACLMessage msg =new ACLMessage(ACLMessage.INFORM);
         msg.addReceiver(new AID("A3", AID.ISLOCALNAME));
-        msg.setContent(Integer.toString(A));
+        try {
+            msg.setContentObject(A);
+        }
+         catch (IOException e) {
+            e.printStackTrace();
+        }
+
         this.myAgent.send(msg);
         System.out.println("Message sent");
 
--- src/Behaviour3.java
+++ src/Behaviour3.java
@@ -1,5 +1,6 @@
 import jade.core.behaviours.Behaviour;
 import jade.lang.acl.ACLMessage;
+import jade.lang.acl.UnreadableException;
 
 public class Behaviour3 extends Behaviour {
     int step =0;
@@ -7,18 +8,36 @@ public class Behaviour3 extends Behaviour {
     public void action () {
         System.out.println("MY Name is"+ this.myAgent.getLocalName());
 
-        switch (step)
-        {
-            case 0,1,2: System.out.println("STEP" + step);step++; break;
+        switch (step) {
+            case 0, 1, 2 -> {
+                System.out.println("STEP" + step);
+                step++;
+            }
         }
 
         System.out.println("This is a Generic Behaviour");
+//receiving a table
+        ACLMessage msg;
+        msg= this.myAgent.blockingReceive();
+        if (null != msg)
+            try {
+               int[] A = (int[]) msg.getContentObject();
+                System.out.println("Message recu ");
+                int i=0;
+                for(i=0;i<A.length;i++){ System.out.print(" "+A[i]+" ");}
+                System.out.println(" ");
 
+            } catch (UnreadableException e) {
+                e.printStackTrace();
+            }
+
+       /*
         ACLMessage msg;
         msg= this.myAgent.receive();
         if (msg != null)
         System.out.println("Message recu ");
-       /* msg= this.myAgent.Blokingreceive();
+       */
+        /* msg= this.myAgent.Blokingreceive();
         if (msg.getcontent() != null)
         System.out.println("Message recu ");
        */
